216d4454e99649f889ec4e63421273c21f91dbd6
fintan-smyth <fintan.smyth@ymail.com>
2025-03-28 00:08:39 +0000

    removed exit logging

--- src/app/cleanup.c
+++ src/app/cleanup.c
@@ -18,20 +18,20 @@ int	cleanup(t_info *app)
 	mlx_destroy_window(app->mlx, app->root);
 	free(app->mlx);
 	free_map(app->map);
-	for (int i = 0; i < WIN_WIDTH; i++)
-	{
-		char	face_char;
-
-		if (app->player->rays[i].face == NORTH)
-			face_char = 'N';
-		else if (app->player->rays[i].face == SOUTH)
-			face_char = 'S';
-		else if (app->player->rays[i].face == EAST)
-			face_char = 'E';
-		else
-			face_char = 'W';
-		dprintf(2, "(%f, %f) %c dist: %f\n", app->player->rays[i].intcpt.x, app->player->rays[i].intcpt.y, face_char, app->player->rays[i].distance);
-	}
+	// for (int i = 0; i < WIN_WIDTH; i++)
+	// {
+	// 	char	face_char;
+	//
+	// 	if (app->player->rays[i].face == NORTH)
+	// 		face_char = 'N';
+	// 	else if (app->player->rays[i].face == SOUTH)
+	// 		face_char = 'S';
+	// 	else if (app->player->rays[i].face == EAST)
+	// 		face_char = 'E';
+	// 	else
+	// 		face_char = 'W';
+	// 	dprintf(2, "(%f, %f) %c dist: %f\n", app->player->rays[i].intcpt.x, app->player->rays[i].intcpt.y, face_char, app->player->rays[i].distance);
+	// }
 	free(app->player);
 	printf("\e[?25h");
 	return (0);
