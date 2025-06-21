f3c56c1217545d718a78153f8ca2cffd5c7785ad
Andrei Belov <abelov@student.42london.com>
2025-06-22 00:45:46 +0100

    cub3d: fill_floor_transposed. Yay!
    Signed-off-by: Andrei Belov <abelov@student.42london.com>

--- src/render/floor.c
+++ src/render/floor.c
@@ -43,7 +43,8 @@ int	get_tex_index_float(double pos, int dim)
 static inline __attribute__((always_inline, unused))
 int	point_oob(t_vect pos, t_lvl *lvl)
 {
-	return ((pos.x < 0 || pos.x > lvl->width) || (pos.y < 0 || pos.y > lvl->height));
+	return ((pos.x < 0 || pos.x > lvl->width)
+		|| (pos.y < 0 || pos.y > lvl->height));
 }
 
 static inline __attribute__((always_inline, unused))
@@ -157,62 +158,72 @@ void	fill_floor(t_info *app, t_player *player, int is_floor)
 	}
 }
 
-static inline __attribute__((always_inline))
-void	draw_floor_row_transposed(t_vect pos[2], u_int *dst, t_img *tex, int row)
-{
-	int				col;
-	t_vect			step;
-	t_vect			curr;
-	t_ivect			idx;
-	u_int			*src = (u_int *)tex->data;
-	int				width = tex->width;
-	int				height = tex->height;
-
-	curr = pos[LEFT];
-	step.x = (pos[RIGHT].x - pos[LEFT].x) / WIN_WIDTH * 2;
-	step.y = (pos[RIGHT].y - pos[LEFT].y) / WIN_WIDTH * 2;
-
-	col = 0;
-	while (col < WIN_WIDTH)
-	{
-		idx.x = ((int)(curr.x * width)) & (width - 1);
-		idx.y = ((int)(curr.y * height)) & (height - 1);
-		u_int color = src[idx.y * width + idx.x];
-
-		int trans_idx_0 = (col + 0) * (WIN_HEIGHT / 2) + (WIN_HEIGHT / 2 - row - 1);
-		int trans_idx_1 = (col + 1) * (WIN_HEIGHT / 2) + (WIN_HEIGHT / 2 - row - 1);
-
-		dst[trans_idx_0] = color;
-		dst[trans_idx_1] = color;
-
-		curr.x += step.x * 2;
-		curr.y += step.y * 2;
-		col += 2;
-	}
-}
-
+inline __attribute__((used))
 void	fill_floor_transposed(t_info *app, t_player *player, int is_floor)
 {
 	t_vect	dir[2];
 	t_vect	pos[2];
-	u_int	*dst = (u_int *)app->canvas_r->data; // Transposed canvas (same buffer)
-	t_img	*tex = app->lvl->planes[(t_enum_tex[]){T_CEILING, T_FLOOR}[is_floor]];
-	int		row;
+	t_vect	step, curr;
+	t_ivect idx;
+	t_cdata row;
+	t_ivect iter;
+
+	int depths_step;
+	int y;
+	int y_start;
+	int depths_idx_start;
 	double	depth;
+	t_img	tex;
+
+	bool is_ceil = !is_floor;
 
 	dir[LEFT] = rotate_vect(player->dir, app->fov_rad_half);
 	dir[RIGHT] = rotate_vect(player->dir, -app->fov_rad_half);
 
-	row = -1;
-	while (++row < WIN_HEIGHT / 2)
+	if (is_ceil)
 	{
-		depth = is_floor
-				? player->row_depths[row]
-				: player->row_depths[WIN_HEIGHT / 2 - row - 1];
+		tex = *app->lvl->planes[T_CEILING];
+		y_start = 0;
+		depths_idx_start = 0;
+		depths_step = 1;
+	}
+	else
+	{
+		tex = *app->lvl->planes[T_FLOOR];
+		y_start = WIN_HEIGHT / 2;
+		depths_idx_start = WIN_HEIGHT / 2 - 1;
+		depths_step = -1;
+	}
+
+	row.dst = (int *)app->canvas_r->data;
+	row.src = (int *)tex.data;
+
+	iter.y = -1;
+	while (++iter.y < WIN_HEIGHT / 2)
+	{
+		y = y_start + iter.y;
+		depth = player->row_depths[iter.y * depths_step + depths_idx_start];
 
 		pos[LEFT] = add_vect(player->pos, scale_vect(dir[LEFT], depth));
 		pos[RIGHT] = add_vect(player->pos, scale_vect(dir[RIGHT], depth));
 
-		draw_floor_row_transposed(pos, dst, tex, row);
+		step.x = (pos[RIGHT].x - pos[LEFT].x) / WIN_WIDTH * 2;
+		step.y = (pos[RIGHT].y - pos[LEFT].y) / WIN_WIDTH * 2;
+
+		curr = pos[LEFT];
+		iter.x = 0;
+		while (iter.x < WIN_WIDTH - 1)
+		{
+			idx.x = ((int)(curr.x * tex.width)) & (tex.width - 1);
+			idx.y = ((int)(curr.y * tex.height)) & (tex.height - 1);
+
+			row.dst[iter.x * WIN_HEIGHT + y] = row.src[idx.y * tex.width + idx.x];
+			row.dst[(iter.x + 1) * WIN_HEIGHT + y] = row.src[idx.y * tex.width + idx.x];
+
+			curr.x += step.x;
+			curr.y += step.y;
+
+			iter.x += 2;
+		}
 	}
 }
