41037961e36dbbb0f34f9b1dbeb587aaa69c0922
Andrei Belov <abelov@student.42london.com>
2025-06-30 23:28:05 +0100

    cub3d: fill_floor_sse4x4: calculate final_idxss
    Signed-off-by: Andrei Belov <abelov@student.42london.com>

--- src/render/floor_avx2.c
+++ src/render/floor_avx2.c
@@ -105,16 +105,19 @@ void	fill_floor_sse4x4(t_info *app, t_player *player)
 
 	t_vect	step;
 	t_vect	curr;
-	float	currs_x[4];
-	float	currs_y[4];
-	float	scaled_x[4];
-	float	scaled_y[4];
-//	t_ivect	idx;
-	t_ivect	idxs[4];
+//	t_ivect	idxs[4];
+	int	idxs_x[4];
+	int	idxs_y[4];
 	int		src_pixels[4];
 	int		full_src_pixels[8];
 
 	tex = *app->lvl->planes[T_FLOOR];
+
+	__m128 tex_width = _mm_set1_ps(tex.width);
+	__m128 tex_height = _mm_set1_ps(tex.height);
+	__m128i tex_width_i = _mm_set1_epi32(tex.width);
+	__m128i tex_width_mask = _mm_set1_epi32(tex.width - 1);
+	__m128i tex_height_mask = _mm_set1_epi32(tex.height - 1);
 
 	it.y = -1;
 	while (++it.y < WIN_HEIGHT / 2)
@@ -123,6 +126,9 @@ void	fill_floor_sse4x4(t_info *app, t_player *player)
 
 		pos[LEFT] = add_vect(player->pos, scale_vect(dir[LEFT], depth));
 		pos[RIGHT] = add_vect(player->pos, scale_vect(dir[RIGHT], depth));
+
+		__m128  pos_left_xx = _mm_set1_ps(pos[LEFT].x);
+		__m128  pos_left_yy = _mm_set1_ps(pos[LEFT].y);
 
 		row.dst = (int *)app->canvas->data + (it.y + WIN_HEIGHT / 2) * app->canvas->width;
 		row.src = (int *)tex.data;
@@ -141,42 +147,35 @@ void	fill_floor_sse4x4(t_info *app, t_player *player)
 		{
 			__m128i it_xx = _mm_set1_epi32(it.x);
 			__m128  scaled_xx = _mm_mul_ps(step_xx, _mm_cvtepi32_ps(_mm_add_epi32( it_xx, initial)));
-			_mm_storeu_ps(scaled_x, scaled_xx);
+			__m128  scaled_yy = _mm_mul_ps(step_yy, _mm_cvtepi32_ps(_mm_add_epi32( it_xx, initial)));
 
-//			scaled_x[0] = step.x * (it.x + 0);
-//			scaled_x[1] = step.x * (it.x + 2);
-//			scaled_x[2] = step.x * (it.x + 4);
-//			scaled_x[3] = step.x * (it.x + 6);
+			__m128  currs_xx = _mm_add_ps(pos_left_xx, scaled_xx);
+			__m128  currs_yy = _mm_add_ps(pos_left_yy, scaled_yy);
 
-			scaled_y[0] = step.y * (it.x + 0);
-			scaled_y[1] = step.y * (it.x + 2);
-			scaled_y[2] = step.y * (it.x + 4);
-			scaled_y[3] = step.y * (it.x + 6);
+			__m128i idxs_xx = _mm_cvttps_epi32(_mm_mul_ps(currs_xx, tex_width));
+			__m128i idxs_yy = _mm_cvttps_epi32(_mm_mul_ps(currs_yy, tex_height));
 
-			currs_x[0] = pos[LEFT].x + scaled_x[0];
-			currs_x[1] = pos[LEFT].x + scaled_x[1];
-			currs_x[2] = pos[LEFT].x + scaled_x[2];
-			currs_x[3] = pos[LEFT].x + scaled_x[3];
+			idxs_xx = _mm_and_si128(idxs_xx, tex_width_mask);
+			idxs_yy = _mm_and_si128(idxs_yy, tex_height_mask);
 
-			currs_y[0] = pos[LEFT].y + scaled_y[0];
-			currs_y[1] = pos[LEFT].y + scaled_y[1];
-			currs_y[2] = pos[LEFT].y + scaled_y[2];
-			currs_y[3] = pos[LEFT].y + scaled_y[3];
+			_mm_storeu_si128((__m128i_u *) idxs_x, idxs_xx);
+			_mm_storeu_si128((__m128i_u *) idxs_y, idxs_yy);
 
-			idxs[0].x = ((int) (currs_x[0] * tex.width)) & (tex.width - 1);
-			idxs[1].x = ((int) (currs_x[1] * tex.width)) & (tex.width - 1);
-			idxs[2].x = ((int) (currs_x[2] * tex.width)) & (tex.width - 1);
-			idxs[3].x = ((int) (currs_x[3] * tex.width)) & (tex.width - 1);
+			int final_idxs[4];
 
-			idxs[0].y = ((int) (currs_y[0] * tex.height)) & (tex.height - 1);
-			idxs[1].y = ((int) (currs_y[1] * tex.height)) & (tex.height - 1);
-			idxs[2].y = ((int) (currs_y[2] * tex.height)) & (tex.height - 1);
-			idxs[3].y = ((int) (currs_y[3] * tex.height)) & (tex.height - 1);
+			__m128i final_idxss = _mm_add_epi32(_mm_mullo_epi32(idxs_yy, tex_width_i), idxs_xx);
 
-			src_pixels[0] = row.src[idxs[0].y * tex.width + idxs[0].x];
-			src_pixels[1] = row.src[idxs[1].y * tex.width + idxs[1].x];
-			src_pixels[2] = row.src[idxs[2].y * tex.width + idxs[2].x];
-			src_pixels[3] = row.src[idxs[3].y * tex.width + idxs[3].x];
+			_mm_storeu_si128((__m128i_u *) final_idxs, final_idxss);
+
+			final_idxs[0] = idxs_y[0] * tex.width + idxs_x[0];
+			final_idxs[1] = idxs_y[1] * tex.width + idxs_x[1];
+			final_idxs[2] = idxs_y[2] * tex.width + idxs_x[2];
+			final_idxs[3] = idxs_y[3] * tex.width + idxs_x[3];
+
+			src_pixels[0] = row.src[final_idxs[0]];
+			src_pixels[1] = row.src[final_idxs[1]];
+			src_pixels[2] = row.src[final_idxs[2]];
+			src_pixels[3] = row.src[final_idxs[3]];
 
 			full_src_pixels[7] = src_pixels[3];
 			full_src_pixels[6] = src_pixels[3];
@@ -195,6 +194,7 @@ void	fill_floor_sse4x4(t_info *app, t_player *player)
 			it.x += 2 * 4;
 		}
 
+//		t_ivect	idx;
 //		while (it.x < WIN_WIDTH - 1)
 //		{
 //
