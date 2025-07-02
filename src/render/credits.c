2b237bde6a47aa4edeb449f52ea8af958760b163
Andrei Belov <abelov@student.42london.com>
2025-07-02 01:15:13 +0100

    cub3d: dim_colour2_vec
    Signed-off-by: Andrei Belov <abelov@student.42london.com>

--- src/render/credits.c
+++ src/render/credits.c
@@ -24,8 +24,8 @@
 static inline __attribute((always_inline))
 t_colour lerp_biased_c(t_colour a, t_colour b, double t)
 {
-	t_colour	result;
-	uint32_t	mask;
+	t_colour result;
+	uint32_t mask;
 
 	mask = -(a.a < b.a);
 	// Choose RGB from more opaque color (lower alpha)
@@ -37,8 +37,8 @@ t_colour lerp_biased_c(t_colour a, t_colour b, double t)
 static inline __attribute((always_inline, unused))
 u_int lerp_biased(u_int aa, u_int bb, float t)
 {
-	t_colour	result;
-	uint32_t	mask;
+	t_colour result;
+	uint32_t mask;
 
 	t_colour a = {.raw = aa};
 	t_colour b = {.raw = bb};
@@ -51,14 +51,14 @@ u_int lerp_biased(u_int aa, u_int bb, float t)
 }
 
 static inline __attribute__((always_inline, unused))
-t_colour	linear_filter(t_vect idx, const t_tex *tex)
+t_colour linear_filter(t_vect idx, const t_tex *tex)
 {
-	const double	frac = fmod(idx.x, 1.0);
+	const double frac = fmod(idx.x, 1.0);
 
 	int x = (int) idx.y * tex->w + (int) (idx.x);
 
-	t_colour left = *(t_colour *)&tex->data[x];
-	t_colour right = *(t_colour *)&tex->data[x + 1];
+	t_colour left = *(t_colour *) &tex->data[x];
+	t_colour right = *(t_colour *) &tex->data[x + 1];
 
 	t_colour out = lerp_biased_c(left, right, frac);
 	return (out);
@@ -66,16 +66,17 @@ t_colour	linear_filter(t_vect idx, const t_tex *tex)
 
 
 static inline __attribute__((always_inline, unused))
-int	interpolate_colour_inline(int col1, int col2, double frac)
+int interpolate_colour_inline(int col1, int col2, double frac)
 {
-	int	r;
-	int	g;
-	int	b;
+	int r;
+	int g;
+	int b;
 
 	if (col1 != col2 && col1 != (int) XPM_TRANSPARENT)
 	{
 		r = ((col2 & MLX_RED) - (col1 & MLX_RED)) * frac + (col1 & MLX_RED);
-		g = ((col2 & MLX_GREEN) - (col1 & MLX_GREEN)) * frac + (col1 & MLX_GREEN);
+		g = ((col2 & MLX_GREEN) - (col1 & MLX_GREEN)) * frac +
+			(col1 & MLX_GREEN);
 		b = ((col2 & MLX_BLUE) - (col1 & MLX_BLUE)) * frac + (col1 & MLX_BLUE);
 		col1 = (r & MLX_RED) + (g & MLX_GREEN) + b;
 	}
@@ -98,10 +99,10 @@ int	interpolate_colour_inline(int col1, int col2, double frac)
  */
 //inline __attribute__((always_inline, unused, visibility("hidden")))
 static inline __attribute__((optnone, used))
-t_colour	bilinear_filter(t_vect idx, const t_tex *tex)
+t_colour bilinear_filter(t_vect idx, const t_tex *tex)
 {
-	const int x = (int)idx.x;
-	const int y = (int)idx.y;
+	const int x = (int) idx.x;
+	const int y = (int) idx.y;
 
 	const double frac_x = idx.x - x;
 	const double frac_y = idx.y - y;
@@ -114,22 +115,22 @@ t_colour	bilinear_filter(t_vect idx, const t_tex *tex)
 	u_int *row2 = tex->data + y1 * tex->w;
 
 	u_int top = lerp_biased(row1[x], row1[x1], frac_x);
-	u_int bottom  = lerp_biased(row2[x], row2[x1], frac_x);
+	u_int bottom = lerp_biased(row2[x], row2[x1], frac_x);
 	t_colour out = {.raw = lerp_biased(top, bottom, frac_y)};
 
 	return (out);
 }
 
 static inline __attribute__((always_inline, unused))
-int	bilinear_filter_old(double x, double y, const t_tex *tex)
+int bilinear_filter_old(double x, double y, const t_tex *tex)
 {
-	int		x_lower;
-	int		x_upper;
-	int		y_lower;
-	int		y_upper;
+	int x_lower;
+	int x_upper;
+	int y_lower;
+	int y_upper;
 
-	x_lower = (int)x;
-	y_lower = (int)y;
+	x_lower = (int) x;
+	y_lower = (int) y;
 	x_upper = x_lower + 1;
 	y_upper = y_lower + 1;
 
@@ -138,8 +139,8 @@ int	bilinear_filter_old(double x, double y, const t_tex *tex)
 	if (x_upper == tex->w)
 		x_upper = 0;
 
-	int col1 = (int)tex->data[y_lower * tex->w + x_lower];
-	int col2 = (int)tex->data[y_lower * tex->w + x_upper];
+	int col1 = (int) tex->data[y_lower * tex->w + x_lower];
+	int col2 = (int) tex->data[y_lower * tex->w + x_upper];
 	if (y_upper != tex->h)
 	{
 		int col3 = (int) tex->data[y_upper * tex->w + x_lower];
@@ -153,7 +154,7 @@ int	bilinear_filter_old(double x, double y, const t_tex *tex)
 }
 
 static inline __attribute__((always_inline, unused))
-t_colour	dim_colour_alpha(t_colour src, double falloff)
+t_colour dim_colour_alpha(t_colour src, double falloff)
 {
 	if (falloff < 1 || src.raw == XPM_TRANSPARENT)
 		return (src);
@@ -165,9 +166,9 @@ t_colour	dim_colour_alpha(t_colour src, double falloff)
 	alpha = alpha > 228 ? 255 : alpha;
 
 	src.a = (u_char) alpha;
-	src.r = (u_char)(src.r * dim);
-	src.g = (u_char)(src.g / falloff);
-	src.b = (u_char)(src.b / falloff);
+	src.r = (u_char) (src.r * dim);
+	src.g = (u_char) (src.g * dim);
+	src.b = (u_char) (src.b * dim);
 	return (src);
 }
 
@@ -179,71 +180,127 @@ t_colour	dim_colour_alpha(t_colour src, double falloff)
  * @param dim
  * @return
  */
-static inline __attribute__((always_inline))
-u_int dim_colour2(u_int col, double dim)
+static inline __attribute__((unused))
+u_int dim_colour2_scal(u_int col, double dim)
 {
+	u_int result;
 	t_colour src = {.raw = col};
-	u_int mask = -(col == XPM_TRANSPARENT);
 
 	dim = fmax(0.0, fmin(1.0, dim));
 
 	double alpha = (1.0 - dim) * 255;
 
-	src.r = (u_char)(src.r * dim);
-	src.g = (u_char)(src.g * dim);
-	src.b = (u_char)(src.b * dim);
-	src.a = (u_char)(alpha);
+	src.a = (u_char) (alpha);
+	src.r = (u_char) (src.r * dim);
+	src.g = (u_char) (src.g * dim);
+	src.b = (u_char) (src.b * dim);
 
-	return (src.raw & ~mask) | (col & mask);
+	u_int mask = -(col == XPM_TRANSPARENT);
+	result = (src.raw & ~mask) | (col & mask);
+	return (result);
 }
 
-void	draw_credits_row(t_info *app, t_vect l_pos, t_vect r_pos, int row)
+static inline __attribute__((optnone))
+__m128i	dim_colour2_vec(__m128i color_vec, float dim)
 {
-	const t_tex		*tex = &app->shtex->credits;
-	int				i;
-	double			step_x;
-	double			curr_x;
-	t_vect			idx;
-	double			dist = app->dummy->row_depths[row - 1];
-	t_vect			lim = {-0.48,  0.48}; // Relative to 1 block on the map
-	u_int *const	p_row = (u_int *)app->overlay->data + app->overlay->width * row;
+	t_m128i			mc = {.src = color_vec};
+	mc.transparent = _mm_set1_epi32(XPM_TRANSPARENT);
+
+	dim = fmaxf(0.0f, fminf(1.0f, dim));
+
+	__m128 dim_vec = _mm_set1_ps(dim);
+	__m128 alpha_vec = _mm_set1_ps((1.0f - dim) * 255.0f);
+
+	t_colour src[4];
+	_mm_storeu_si128((__m128i_u *) src, color_vec);
+
+	// Extract channels: each pixel is 0xAARRGGBB
+	int r[4] = { src[0].r, src[1].r, src[2].r, src[3].r };
+	int g[4] = { src[0].g, src[1].g, src[2].g, src[3].g };
+	int b[4] = { src[0].b, src[1].b, src[2].b, src[3].b };
+
+	__m128i aa = _mm_cvttps_epi32(alpha_vec);
+	__m128i rr = _mm_loadu_si128((const __m128i_u *)r);
+	__m128i gg = _mm_loadu_si128((const __m128i_u *)g);
+	__m128i bb = _mm_loadu_si128((const __m128i_u *)b);
+
+	rr = _mm_cvtps_epi32(_mm_mul_ps(_mm_cvtepi32_ps(rr), dim_vec));
+	gg = _mm_cvtps_epi32(_mm_mul_ps(_mm_cvtepi32_ps(gg), dim_vec));
+	bb = _mm_cvtps_epi32(_mm_mul_ps(_mm_cvtepi32_ps(bb), dim_vec));
+
+	int out[4];
+	int a[4];
+
+	_mm_storeu_si128((__m128i_u *) a, aa);
+	_mm_storeu_si128((__m128i_u *) r, rr);
+	_mm_storeu_si128((__m128i_u *) g, gg);
+	_mm_storeu_si128((__m128i_u *) b, bb);
+
+	// Repack back into 0xAARRGGBB
+	out[0] = (t_colour){.a = a[0], .r = r[0], .g = g[0], .b = b[0]}.raw;
+	out[1] = (t_colour){.a = a[1], .r = r[1], .g = g[1], .b = b[1]}.raw;
+	out[2] = (t_colour){.a = a[2], .r = r[2], .g = g[2], .b = b[2]}.raw;
+	out[3] = (t_colour){.a = a[3], .r = r[3], .g = g[3], .b = b[3]}.raw;
+
+	mc.dst = _mm_loadu_si128((const __m128i_u *)out);
+	mc.mask = _mm_cmpeq_epi32(color_vec, mc.transparent);
+	mc.blend = _mm_or_si128(
+		_mm_andnot_si128(mc.mask, mc.dst),
+		_mm_and_si128(mc.mask, mc.src)
+	);
+	return mc.blend;
+}
+
+void draw_credits_row(t_info *app, t_vect l_pos, t_vect r_pos, int row)
+{
+	const t_tex *tex = &app->shtex->credits;
+	int i;
+	double step_x;
+	double curr_x;
+	t_vect idx;
+	double dist = app->dummy->row_depths[row - 1];
+	t_vect lim = {-0.48, 0.48}; // Relative to 1 block on the map
+	u_int *const p_row =
+		(u_int *) app->overlay->data + app->overlay->width * row;
 
 	step_x = (r_pos.x - l_pos.x) / WIN_WIDTH;
 	curr_x = l_pos.x;
 	idx.y = (-l_pos.y) * tex->w;
 	if (l_pos.y > 0 || idx.y > tex->h)
-		return ;
+		return;
 	i = -1;
 	while (++i < WIN_WIDTH)
 	{
 		if (curr_x > lim.x && curr_x < lim.y)
 		{
 			idx.x = (0.5 + curr_x) * tex->w;
-			p_row[i] = dim_colour_alpha(bilinear_filter(idx, tex), (dist - 1.5) * 6).raw;
+			p_row[i] = dim_colour_alpha(bilinear_filter(idx, tex),
+										(dist - 1.5) * 6).raw;
 		}
 		curr_x += step_x;
 	}
 }
 
-void	update_rocks(t_info *app, t_dummy *dummy)
+void update_rocks(t_info *app, t_dummy *dummy)
 {
-	t_rock	*rock;
-	t_list	*current;
+	t_rock *rock;
+	t_list *current;
 
 	current = dummy->rocks;
 	while (current != NULL)
 	{
 		rock = current->data;
-		place_tex_to_image_scale(app->canvas, rock->tex, round_vect(rock->pos), rock->scale);
+		place_tex_to_image_scale(app->canvas, rock->tex, round_vect(rock->pos),
+								 rock->scale);
 		rock->pos.x += rock->speed;
 		current = current->next;
 	}
 }
 
-int	cmp_rock_speed(void *data1, void *data2)
+int cmp_rock_speed(void *data1, void *data2)
 {
-	t_rock	*rock1;
-	t_rock	*rock2;
+	t_rock *rock1;
+	t_rock *rock2;
 
 	rock1 = data1;
 	rock2 = data2;
@@ -252,10 +309,10 @@ int	cmp_rock_speed(void *data1, void *data2)
 	return (-1);
 }
 
-void	spawn_rock(t_info *app, t_vect pos, t_tex *tex, double speed)
+void spawn_rock(t_info *app, t_vect pos, t_tex *tex, double speed)
 {
-	t_rock	*rock;
-	
+	t_rock *rock;
+
 	rock = ft_calloc(1, sizeof(*rock));
 	rock->pos = pos;
 	rock->speed = speed;
@@ -263,10 +320,10 @@ void	spawn_rock(t_info *app, t_vect pos, t_tex *tex, double speed)
 	ft_lstadd_back(&app->dummy->rocks, ft_lstnew(rock));
 }
 
-void	spawn_random_rock(t_info *app, double speed)
+void spawn_random_rock(t_info *app, double speed)
 {
-	t_rock	*rock;
-	int		index;
+	t_rock *rock;
+	int index;
 
 	rock = ft_calloc(1, sizeof(*rock));
 	rock->scale = 3;
@@ -289,7 +346,8 @@ void	spawn_random_rock(t_info *app, double speed)
 		if (fabs(speed) < 1.5)
 			rock->scale = 2;
 	}
-	rock->pos.y = ((WIN_HEIGHT - 80) / 12.0) * fabs(speed) * 2 + rand_range(-100, 100);
+	rock->pos.y =
+		((WIN_HEIGHT - 80) / 12.0) * fabs(speed) * 2 + rand_range(-100, 100);
 	if (rock->pos.y < 0)
 		rock->pos.y = rand_range(0, 100);
 	if (rock->pos.y > WIN_HEIGHT)
@@ -298,13 +356,13 @@ void	spawn_random_rock(t_info *app, double speed)
 	ft_lstadd_back(&app->dummy->rocks, ft_lstnew(rock));
 }
 
-void	draw_credits(t_info *app, t_dummy *dummy)
+void draw_credits(t_info *app, t_dummy *dummy)
 {
-	t_vect	l_dir;
-	t_vect	r_dir;
-	t_vect	l_pos;
-	t_vect	r_pos;
-	int		row;
+	t_vect l_dir;
+	t_vect r_dir;
+	t_vect l_pos;
+	t_vect r_pos;
+	int row;
 
 	row = 0;
 	l_dir = rotate_vect(dummy->dir, app->fov_rad_half);
@@ -312,75 +370,78 @@ void	draw_credits(t_info *app, t_dummy *dummy)
 	update_rocks(app, dummy);
 	while (++row < WIN_HEIGHT)
 	{
-		l_pos = add_vect(dummy->pos, scale_vect(l_dir, dummy->row_depths[row - 1]));
-		r_pos = add_vect(dummy->pos, scale_vect(r_dir, dummy->row_depths[row - 1]));
+		l_pos = add_vect(dummy->pos,
+						 scale_vect(l_dir, dummy->row_depths[row - 1]));
+		r_pos = add_vect(dummy->pos,
+						 scale_vect(r_dir, dummy->row_depths[row - 1]));
 		draw_credits_row(app, l_pos, r_pos, row);
 	}
 	place_img_alpha_sse(app->canvas, app->overlay, (t_point) {0, 0});
 }
 
-void	draw_credits_avx2(t_info *app, t_dummy *dummy)
+void draw_credits_avx2(t_info *app, t_dummy *dummy)
 {
-	t_vect	dir[2];
-	t_vect	pos[2];
-	int		row;
+	t_vect dir[2];
+	t_vect pos[2];
+	int row;
 
 
-
-	int		i;
+	int i;
 
 //	double	depth;
 	const t_tex *tex = &app->shtex->credits;
+
+	__m128 half_ps = _mm_set1_ps(0.5f);
+	__m128 one_ps = _mm_set1_ps(1.0f);
+	__m128i one_epi32 = _mm_set1_epi32(1);
+	__m128i max_val = _mm_set1_epi32(tex->w - 1);
 	row = 0;
 	dir[LEFT] = rotate_vect(dummy->dir, app->fov_rad_half);
 	dir[RIGHT] = rotate_vect(dummy->dir, -app->fov_rad_half);
 	update_rocks(app, dummy);
 	while (++row < WIN_HEIGHT)
 	{
-		double depth = app->dummy->row_depths[row - 1];
+		float depth = app->dummy->row_depths[row - 1];
 
 		pos[LEFT] = add_vect(dummy->pos, scale_vect(dir[LEFT], depth));
 		pos[RIGHT] = add_vect(dummy->pos, scale_vect(dir[RIGHT], depth));
 
 		__m128 pos_LEFT_x = _mm_set1_ps(pos[LEFT].x);
-		__m128 half_ps = _mm_set1_ps(0.5);
 
 		float step_x;
-//		double step_y;
 		float curr_x;
-		float idx_xs[4];
-		float idx_ys[4];
-//		double curr_y;
 
+		const float falloff = (depth - 1.5f) * 6.0f;
 
-		const double falloff = (depth - 1.5) * 6.0;
-
-//		double dim = falloff >= 1.0 ? 1.0 / falloff : 1.0;
+//		float dim = falloff >= 1.0 ? 1.0 / falloff : 1.0;
 
 		float inv = 1.0 / (falloff + DBL_EPSILON);
 		float dim = 1.0 + (inv - 1.0) * (falloff >= 1.0);
 
 		t_vect lim = {-0.48, 0.48}; // Relative to 1 block on the map
 
-		u_int *const p_row = (u_int *) app->overlay->data + app->overlay->width * row;
+		u_int *const p_row =
+			(u_int *) app->overlay->data + app->overlay->width * row;
 
 		step_x = (pos[RIGHT].x - pos[LEFT].x) / WIN_WIDTH;
 //		step_y = 0;
 		curr_x = pos[LEFT].x;
 //		curr_y = pos[LEFT].y * 0;
 		float idx_y = (-pos[LEFT].y) * tex->w;
-		__m128i  idx_yy = _mm_set1_ps(idx_y);
 		float weight_y = fmodf(idx_y, 1.0f);
+
+		__m128 weight_yy = _mm_set1_ps(weight_y);
 
 		if (pos[LEFT].y > 0)
 			continue;
 		if (idx_y > tex->h)
-			break ;
+			break;
 
-		int y = (int)idx_y;
-		int y1 = y + ((((tex->h - 1) - (y + 1)) >> 31) ^ 1); // y1 = MIN(y + 1, tex->h - 1);
+		int y = (int) idx_y;
+		int y1 = y + ((((tex->h - 1) - (y + 1)) >> 31) ^
+					  1); // y1 = MIN(y + 1, tex->h - 1);
 
-		int start = MAX(0, (lim.x - pos[LEFT].x) / step_x );
+		int start = MAX(0, (lim.x - pos[LEFT].x) / step_x);
 		int stop = MIN(WIN_WIDTH, (lim.y - pos[LEFT].x) / step_x);
 //		int stop = MIN(WIN_WIDTH, (int)(step_x * WIN_WIDTH));
 
@@ -391,102 +452,76 @@ void	draw_credits_avx2(t_info *app, t_dummy *dummy)
 		{
 			curr_x = pos[LEFT].x + (step_x * i);
 
-			__m128i  initial = _mm_setr_epi32(0, 1, 2, 3);
-			__m128i  ii = _mm_set1_epi32(i);
+			__m128i initial = _mm_setr_epi32(0, 1, 2, 3);
+			__m128i ii = _mm_set1_epi32(i);
 
-			__m128  step_xx = _mm_set1_ps(step_x);
-			__m128  scaled_xx = _mm_mul_ps(step_xx, _mm_cvtepi32_ps(_mm_add_epi32(ii, initial)));
+			__m128 step_xx = _mm_set1_ps(step_x);
+			__m128 scaled_xx = _mm_mul_ps(step_xx, _mm_cvtepi32_ps(
+				_mm_add_epi32(ii, initial)));
 
-			__m128 currs_xx = _mm_add_ps(half_ps, _mm_add_ps(pos_LEFT_x, scaled_xx));
-			__m128 idx_xx = _mm_mul_ps(currs_xx, _mm_cvtepi32_ps(_mm_set1_epi32(tex->w)));
-
-			_mm_storeu_si128((__m128i_u *) idx_xs, idx_xx);
-			_mm_storeu_si128((__m128i_u *) idx_ys, idx_yy);
+			__m128 currs_xx = _mm_add_ps(half_ps,
+										 _mm_add_ps(pos_LEFT_x, scaled_xx));
+			__m128 idx_xx = _mm_mul_ps(currs_xx,
+									   _mm_cvtepi32_ps(_mm_set1_epi32(tex->w)));
 
 			/* ===============bilinear_filter=============== */
 
+			int x1s[4];
 			int xs[4];
-
-			xs[0] = (int) idx_xs[0];
-			xs[1] = (int) idx_xs[1];
-			xs[2] = (int) idx_xs[2];
-			xs[3] = (int) idx_xs[3];
-
 
 			float weight_xs[4];
 			float weight_ys[4];
 
-			weight_xs[0] = idx_xs[0] - (float)xs[0];
-			weight_xs[1] = idx_xs[1] - (float)xs[1];
-			weight_xs[2] = idx_xs[2] - (float)xs[2];
-			weight_xs[3] = idx_xs[3] - (float)xs[3];
+			// fmodf(x, y) == (x - y * trunc(x / y))
+			__m128 div = _mm_div_ps(idx_xx, one_ps);
+			__m128 trunc_div = _mm_round_ps(div, _MM_FROUND_TO_ZERO |
+												 _MM_FROUND_NO_EXC);
+			__m128 prod = _mm_mul_ps(one_ps, trunc_div);
+			__m128 weight_xx = _mm_sub_ps(idx_xx, prod);
 
-			weight_ys[0] = weight_y;
-			weight_ys[1] = weight_y;
-			weight_ys[2] = weight_y;
-			weight_ys[3] = weight_y;
+			_mm_storeu_si128((__m128i_u *) weight_xs, weight_xx);
+			_mm_storeu_si128((__m128i_u *) weight_ys, weight_yy);
 
+			__m128i xx = _mm_cvttps_epi32(idx_xx);
+			__m128i x_plus_1 = _mm_add_epi32(xx, one_epi32);
+			__m128i xx1 = _mm_min_epi32(x_plus_1, max_val);
 
-			int x1s[4];
-
-			x1s[0] = xs[0] + ((((tex->w - 1) - (xs[0] + 1)) >> 31) ^ 1); // x1 = MIN(x + 1, tex->w - 1);
-			x1s[1] = xs[1] + ((((tex->w - 1) - (xs[1] + 1)) >> 31) ^ 1); // x1 = MIN(x + 1, tex->w - 1);
-			x1s[2] = xs[2] + ((((tex->w - 1) - (xs[2] + 1)) >> 31) ^ 1); // x1 = MIN(x + 1, tex->w - 1);
-			x1s[3] = xs[3] + ((((tex->w - 1) - (xs[3] + 1)) >> 31) ^ 1); // x1 = MIN(x + 1, tex->w - 1);
+			_mm_storeu_si128((__m128i_u *) xs, xx);
+			_mm_storeu_si128((__m128i_u *) x1s, xx1);
 
 			// Load the 2x2 texels
-			u_int *row1 = tex->data + y * tex->w;
-			u_int *row2 = tex->data + y1 * tex->w;
+			const int *row1 = (int *) tex->data + y * tex->w;
+			const int *row2 = (int *) tex->data + y1 * tex->w;
 
-			u_int col_lt[4];
-			u_int col_rt[4];
-			u_int col_lb[4];
-			u_int col_rb[4];
+			u_int col_tl_arr[4];
+			u_int col_tr_arr[4];
+			u_int col_bl_arr[4];
+			u_int col_br_arr[4];
 
-			col_lt[0] = row1[xs[0]];
-			col_lt[1] = row1[xs[1]];
-			col_lt[2] = row1[xs[2]];
-			col_lt[3] = row1[xs[3]];
+			__m128i col_tl = _mm_i32gather_epi32((const int *) row1, xx, sizeof(int));
+			__m128i col_tr = _mm_i32gather_epi32((const int *) row1, xx1, sizeof(int));
+			__m128i col_bl = _mm_i32gather_epi32((const int *) row2, xx, sizeof(int));
+			__m128i col_br = _mm_i32gather_epi32((const int *) row2, xx1, sizeof(int));
 
-			col_rt[0] = row1[x1s[0]];
-			col_rt[1] = row1[x1s[1]];
-			col_rt[2] = row1[x1s[2]];
-			col_rt[3] = row1[x1s[3]];
-
-			col_lb[0] = row2[xs[0]];
-			col_lb[1] = row2[xs[1]];
-			col_lb[2] = row2[xs[2]];
-			col_lb[3] = row2[xs[3]];
-
-			col_rb[0] = row2[x1s[0]];
-			col_rb[1] = row2[x1s[1]];
-			col_rb[2] = row2[x1s[2]];
-			col_rb[3] = row2[x1s[3]];
+			_mm_storeu_si128((__m128i_u *) col_tl_arr, col_tl);
+			_mm_storeu_si128((__m128i_u *) col_tr_arr, col_tr);
+			_mm_storeu_si128((__m128i_u *) col_bl_arr, col_bl);
+			_mm_storeu_si128((__m128i_u *) col_br_arr, col_br);
 
 			u_int a[4];
 			u_int b[4];
 			u_int c[4];
 			u_int d[4];
 
-			a[0] = dim_colour2(col_lt[0], dim);
-			a[1] = dim_colour2(col_lt[1], dim);
-			a[2] = dim_colour2(col_lt[2], dim);
-			a[3] = dim_colour2(col_lt[3], dim);
+			__m128i dimmed_a = dim_colour2_vec(col_tl, dim);
+			__m128i dimmed_b = dim_colour2_vec(col_tr, dim);
+			__m128i dimmed_c = dim_colour2_vec(col_bl, dim);
+			__m128i dimmed_d = dim_colour2_vec(col_br, dim);
 
-			b[0] = dim_colour2(col_rt[0], dim);
-			b[1] = dim_colour2(col_rt[1], dim);
-			b[2] = dim_colour2(col_rt[2], dim);
-			b[3] = dim_colour2(col_rt[3], dim);
-
-			c[0] = dim_colour2(col_lb[0], dim);
-			c[1] = dim_colour2(col_lb[1], dim);
-			c[2] = dim_colour2(col_lb[2], dim);
-			c[3] = dim_colour2(col_lb[3], dim);
-
-			d[0] = dim_colour2(col_rb[0], dim);
-			d[1] = dim_colour2(col_rb[1], dim);
-			d[2] = dim_colour2(col_rb[2], dim);
-			d[3] = dim_colour2(col_rb[3], dim);
+			_mm_storeu_si128((__m128i_u *) a, dimmed_a);
+			_mm_storeu_si128((__m128i_u *) b, dimmed_b);
+			_mm_storeu_si128((__m128i_u *) c, dimmed_c);
+			_mm_storeu_si128((__m128i_u *) d, dimmed_d);
 
 			u_int top[4];
 			u_int bottom[4];
