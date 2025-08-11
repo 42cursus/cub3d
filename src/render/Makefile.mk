38e96719edf0cf6730ba07757f1a466773c43cad
Fintan Smyth <fsmyth@student.42london.com>
2025-08-11 16:04:36 +0100

    sky.c norm

--- src/render/Makefile.mk
+++ src/render/Makefile.mk
@@ -6,7 +6,7 @@
 #    By: abelov <abelov@student.42london.com>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/03/08 17:02:06 by abelov            #+#    #+#              #
-#    Updated: 2025/08/11 15:58:22 by fsmyth           ###   ########.fr        #
+#    Updated: 2025/08/11 16:04:04 by fsmyth           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -33,6 +33,7 @@ FILES   = colour.c \
 			render_frame_utils.c \
 			img_to_tex.c \
 			img_to_tex_static.c \
-			sky.c
+			sky.c \
+			replace_sky.c \
 
 CUB_SRCS    += $(FILES:%.c=$(dir $(lastword $(MAKEFILE_LIST)))%.c)
